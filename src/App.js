b3fd3cfe6d84680a7689f601a1e73693e50d373d
LeeJungHoon <gns14585@naver.com>
2023-11-27 12:39:37 +0900

    gns14585/회원가입 진행중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HomeLayout } from "./layout/HomeLayout";
+import { MemberSignup } from "./member/MemberSignup";
+import { MemberLogin } from "./member/MemberLogin";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route path="signup" element={<MemberSignup />} />
+      <Route path="login" element={<MemberLogin />} />
+    </Route>,
+  ),
+);
 
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
