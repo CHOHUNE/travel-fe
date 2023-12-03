82cf91d207a9c2923f77de3b264661dde11fad51
Garbi93 <iseung2093@gmail.com>
2023-12-04 02:12:24 +0900

    12/4 상품 추가 할때에 type 데이터가 2번 들어가는것을 수정 , 메인 이미지 첨부 할수있도록 postForm 타입으로 전송 , 출발 도착지 컬럼 추가 아직 백으로는 구현하지 않음

--- src/page/transport/TransPortWrite.js
+++ src/page/transport/TransPortWrite.js
@@ -1,9 +1,12 @@
 import {
   Box,
   Button,
+  Center,
   Flex,
+  FormControl,
   Heading,
   Input,
+  Select,
   Table,
   Td,
   Th,
@@ -13,7 +16,6 @@ import {
 import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export function TransPortWrite() {
@@ -24,6 +26,13 @@ export function TransPortWrite() {
   const [transSubImage, setTransSubImage] = useState(null);
   const [transContent, setTransContent] = useState(null);
 
+  /* --------------------------------------- 출발지 도착지 경로 컬럼 추가 연습중 --------------------------------------- */
+
+  const [transStartLocation, setTransStartLocation] = useState(null);
+  const [transArriveLocation, setTransArriveLocation] = useState(null);
+
+  /* --------------------------------------- 출발지 도착지 경로 컬럼 추가 연습중 --------------------------------------- */
+
   const navigate = useNavigate();
 
   const toast = useToast();
@@ -32,8 +41,9 @@ export function TransPortWrite() {
   const type = params.get("type");
   function handleSubmitTrans() {
     axios
-      .post("/api/transport/add?" + params.toString(), {
+      .postForm("/api/transport/add", {
         type,
+        transMainImage,
         transStartDay,
         transTitle,
         transPrice,
@@ -57,137 +67,185 @@ export function TransPortWrite() {
   }
 
   return (
-    <Box w={"55%"} ml={"27.5%"} textAlign={"center"} mt={4}>
-      <Heading>상품 등록</Heading>
-      <Table
-        mt={4}
-        border={"1px solid gray"}
-        style={{ borderCollapse: "collapse" }}
-      >
-        <Tr>
-          <Th
-            border={"1px solid gray"}
-            textAlign={"center"}
-            fontSize={"1.1rem"}
-            bg={"#d9d9d9"}
-          >
-            메인 이미지
-          </Th>
-          <Td border={"1px solid gray"}>
-            <Input
-              type={"file"}
-              value={transMainImage}
-              onChange={(e) => setTransMainImage(e.target.value)}
-            />
-          </Td>
-        </Tr>
-        <Tr>
-          <Th
-            border={"1px solid gray"}
-            textAlign={"center"}
-            fontSize={"1.1rem"}
-            bg={"#d9d9d9"}
-          >
-            출발 일시
-          </Th>
-          <Td border={"1px solid gray"}>
-            <Input
-              placeholder="Select Date and Time"
-              size="md"
-              type="datetime-local"
-              onChange={(e) => setTransStartDay(e.target.value)}
-            />
-          </Td>
-        </Tr>
-        <Tr>
-          <Th
-            border={"1px solid gray"}
-            textAlign={"center"}
-            fontSize={"1.1rem"}
-            bg={"#d9d9d9"}
-          >
-            제목
-          </Th>
-          <Td border={"1px solid gray"}>
-            <Input
-              type={"text"}
-              value={transTitle}
-              onChange={(e) => setTransTitle(e.target.value)}
-            />
-          </Td>
-        </Tr>
-        <Tr>
-          <Th
-            border={"1px solid gray"}
-            textAlign={"center"}
-            fontSize={"1.1rem"}
-            bg={"#d9d9d9"}
-          >
-            가격
-          </Th>
-          <Td border={"1px solid gray"}>
-            <Input
-              value={transPrice}
-              onChange={(e) => setTransPrice(e.target.value)}
-            />
-          </Td>
-        </Tr>
-        <Tr>
-          <Th
-            border={"1px solid gray"}
-            textAlign={"center"}
-            fontSize={"1.1rem"}
-            bg={"#d9d9d9"}
-          >
-            상품 설명 이미지₩
-          </Th>
-          <Td border={"1px solid gray"}>
-            <Input
-              type={"file"}
-              value={transSubImage}
-              onChange={(e) => setTransSubImage(e.target.value)}
-            />
-          </Td>
-        </Tr>
-        <Tr>
-          <Th
-            w={"180px"}
+    <Center>
+      <Box w={"55%"} textAlign={"center"} mt={4}>
+        <Heading>상품 등록</Heading>
+        <Table
+          mt={4}
+          border={"1px solid gray"}
+          style={{ borderCollapse: "collapse" }}
+        >
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              메인 이미지
+            </Th>
+            <Td border={"1px solid gray"}>
+              <Input
+                type="file"
+                accept="image/*"
+                onChange={(e) => setTransMainImage(e.target.files[0])}
+              />
+            </Td>
+          </Tr>
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              출발 일시
+            </Th>
+            <Td border={"1px solid gray"}>
+              <Input
+                placeholder="Select Date and Time"
+                size="md"
+                type="datetime-local"
+                onChange={(e) => setTransStartDay(e.target.value)}
+              />
+            </Td>
+          </Tr>
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              제목
+            </Th>
+            <Td border={"1px solid gray"}>
+              <Input
+                type={"text"}
+                value={transTitle}
+                onChange={(e) => setTransTitle(e.target.value)}
+              />
+            </Td>
+          </Tr>
+          {/* --------------------------------------- 출발지 도착지 경로 컬럼 추가 연습중 --------------------------------------- */}
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              출발지
+            </Th>
+            <Td border={"1px solid gray"}>
+              <FormControl>
+                <Select
+                  placeholder="출발 지역을 선택해주세요"
+                  value={transStartLocation}
+                  onChange={(e) => setTransStartLocation(e.target.value)}
+                >
+                  <option>서울</option>
+                  <option>부산</option>
+                </Select>
+              </FormControl>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              도착지
+            </Th>
+            <Td border={"1px solid gray"}>
+              <FormControl>
+                <Select
+                  placeholder="도착 지역을 선택해주세요"
+                  value={transArriveLocation}
+                  onChange={(e) => setTransArriveLocation(e.target.value)}
+                >
+                  <option>서울</option>
+                  <option>부산</option>
+                </Select>
+              </FormControl>
+            </Td>
+          </Tr>
+          {/* --------------------------------------- 출발지 도착지 경로 컬럼 추가 연습중 --------------------------------------- */}
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              가격
+            </Th>
+            <Td border={"1px solid gray"}>
+              <Input
+                value={transPrice}
+                onChange={(e) => setTransPrice(e.target.value)}
+              />
+            </Td>
+          </Tr>
+          <Tr>
+            <Th
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              상품 설명 이미지
+            </Th>
+            <Td border={"1px solid gray"}>
+              <Input
+                type={"file"}
+                value={transSubImage}
+                onChange={(e) => setTransSubImage(e.target.value)}
+              />
+            </Td>
+          </Tr>
+          <Tr>
+            <Th
+              w={"180px"}
+              h={"60px"}
+              border={"1px solid gray"}
+              textAlign={"center"}
+              fontSize={"1.1rem"}
+              bg={"#d9d9d9"}
+            >
+              상품 설명 텍스트
+            </Th>
+            <Td border={"1px solid gray"}>
+              <Input
+                value={transContent}
+                onChange={(e) => setTransContent(e.target.value)}
+              />
+            </Td>
+          </Tr>
+        </Table>
+        <Flex w={"90%"} ml={"5%"} gap={"5%"} mt={8}>
+          <Button
+            w={"50%"}
             h={"60px"}
-            border={"1px solid gray"}
-            textAlign={"center"}
-            fontSize={"1.1rem"}
-            bg={"#d9d9d9"}
+            colorScheme={"white"}
+            color={"black"}
+            border={"1px solid black"}
+            onClick={() => navigate(-1)}
           >
-            상품 설명 텍스트
-          </Th>
-          <Td border={"1px solid gray"}>
-            <Input
-              value={transContent}
-              onChange={(e) => setTransContent(e.target.value)}
-            />
-          </Td>
-        </Tr>
-      </Table>
-      <Flex w={"90%"} ml={"5%"} gap={"5%"} mt={8}>
-        <Button
-          w={"50%"}
-          h={"60px"}
-          colorScheme={"white"}
-          color={"black"}
-          border={"1px solid black"}
-          onClick={() => navigate(-1)}
-        >
-          취소
-        </Button>
-        <Button
-          w={"50%"}
-          h={"60px"}
-          colorScheme={"blue"}
-          onClick={handleSubmitTrans}
-        >
-          저장 하기
-        </Button>
-      </Flex>
-    </Box>
+            취소
+          </Button>
+          <Button
+            w={"50%"}
+            h={"60px"}
+            colorScheme={"blue"}
+            onClick={handleSubmitTrans}
+          >
+            저장 하기
+          </Button>
+        </Flex>
+      </Box>
+    </Center>
   );
 }
