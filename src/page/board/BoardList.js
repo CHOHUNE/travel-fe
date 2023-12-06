e9cefc04af35008dcf8b7ee517ebb06a557b40fa
Jae Hee <travel237corp@naver.com>
2023-12-07 00:12:09 +0900

    게시글 검색기능 (지금은 전체만 됨)

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -13,40 +13,116 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+function Pagination({ pageInfo }) {
+  const pageNumbers = [];
+
+  const navigate = useNavigate();
+
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Box>
+      {pageInfo.prevPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/boardlist?p=" + pageInfo.prevPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </Button>
+      )}
+
+      {pageNumbers.map((pageNumber) => (
+        <Button
+          key={pageNumber}
+          variant={
+            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+          }
+          onClick={() => navigate("/boardlist?p=" + pageNumber)}
+        >
+          {pageNumber}
+        </Button>
+      ))}
+
+      {pageInfo.nextPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/boardlist?p=" + pageInfo.nextPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </Button>
+      )}
+    </Box>
+  );
+}
+
+
+function SearchComponent() {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+
+  function handleSearch() {
+    // /?k=keyword
+    const params = new URLSearchParams();
+    params.set("k", keyword);
+    navigate("/boardList?" + params);
+  }
+  return     (
+    <Box>
+
+
+    <h1> 게시물 목록</h1>
+    <Flex justifyContent={"space-between"}>
+      <select>
+        <option value="Option1">전 체</option>
+        <option value="Option2">제 목</option>
+        <option value="Option3">내 용</option>
+        <option value="Option3">작성자</option>
+      </select>
+      <Input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <Button onClick={handleSearch}>검색</Button>
+    </Flex>
+    </Box>);
+}
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
   const navigate = useNavigate();
+const [params] = useSearchParams();
+  const [pageInfo, setPageInfo] = useState(null);
+  const location = useLocation();
+
 
   useEffect(() => {
     axios
-      .get("/api/board/list")
-      .then((response) => setBoardList(response.data));
-  }, []);
+      .get("/api/board/list?"+params)
+      .then((response) =>{ setBoardList(response.data.boardList);
+      setPageInfo(response.data.pageInfo)});
+
+  }, [location]);
+
+  if (boardList === null) {
+    return <Spinner />;
+  }
+
 
   return (
-    <Box>
-      <Flex>
-        <Button onClick={() => navigate("/boardlist")}>게시판 목록</Button>
-        <Button onClick={() => navigate("/boardwrite")}>게시판 작성</Button>
-        <Button onClick={() => navigate("/signup")}>회원가입</Button>
-        <Button onClick={() => navigate("/member/list")}>회원목록</Button>
-        <Button onClick={() => navigate("/login")}>로그인</Button>
-      </Flex>
 
-      <h1> 게시물 목록</h1>
-      <Flex justifyContent={"space-between"}>
-        <select>
-          <option value="Option1">전 체</option>
-          <option value="Option2">제 목</option>
-          <option value="Option3">내 용</option>
-          <option value="Option3">작성자</option>
-        </select>
-        <Input type="text" />
-        <Button> 검색</Button>
-      </Flex>
-
+<Box>
+  <Flex>
+    <Button onClick={() => navigate("/boardlist")}>게시판 목록</Button>
+    <Button onClick={() => navigate("/boardwrite")}>게시판 작성</Button>
+  </Flex>
+  <SearchComponent />
       <Table>
         <Thead>
           <Tr>
@@ -57,10 +133,7 @@ export function BoardList() {
           </Tr>
         </Thead>
         <Tbody>
-          {boardList === null ? (
-            <Spinner />
-          ) : (
-            boardList.map((board) => (
+          {boardList.map((board) => (
               <Tr
                 key={board.id}
                 _hover={{ cursor: "pointer" }}
@@ -71,10 +144,11 @@ export function BoardList() {
                 <Td>{board.writer}</Td>
                 <Td>{board.inserted}</Td>
               </Tr>
-            ))
+            )
           )}
         </Tbody>
       </Table>
+      <Pagination pageInfo={pageInfo} />
     </Box>
   );
 }
