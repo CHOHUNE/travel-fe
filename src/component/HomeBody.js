64a77227fc3554beac5e57c9374d34781fc52819
LeeJungHoon <gns14585@naver.com>
2023-12-15 14:15:52 +0900

    gns14585/ 메인 작업중

--- src/component/HomeBody.js
+++ src/component/HomeBody.js
@@ -1,5 +1,7 @@
 import {
+  border,
   Box,
+  Button,
   Card,
   CardBody,
   CardHeader,
@@ -9,14 +11,19 @@ import {
   FormLabel,
   Heading,
   Image,
+  Input,
+  VStack,
 } from "@chakra-ui/react";
 import App from "../page/hotel/App";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { RecentViewed } from "./RecentViewed";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+import DatePicker from "react-datepicker";
+import ko from "date-fns/locale/ko";
+import "../component/Calendar.css";
 
 export function HomeBody() {
   const navigate = useNavigate();
@@ -45,6 +52,43 @@ export function HomeBody() {
 
    */
 
+  // ------------------- 날짜 선택 ------------------
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+  const wrapperRef = useRef(null); // 컴포넌트 참조를 위한 ref
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    // 외부 클릭 감지를 위한 이벤트 핸들러
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setIsOpen(false); // 외부 클릭 시 달력 닫기
+      }
+    }
+    // 이벤트 리스너 등록
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
+
+  const handleChange = (dates) => {
+    const [start, end] = dates;
+    setDateRange(dates);
+  };
+
+  const CustomInput = ({ value, onClick }) => (
+    <Button
+      onClick={onClick}
+      bg={"white"}
+      color="black"
+      _hover={{ color: "blue.600" }}
+    >
+      {value || "날짜 선택"}
+    </Button>
+  );
+
   return (
     <Box>
       {/* ---------- 배너이미지  */}
@@ -72,7 +116,108 @@ export function HomeBody() {
       {/* ------------------- 버스상품 중간정렬 ------------------- */}
       <Flex justifyContent="center" w="100%">
         <Box w={"65%"} justifyContent={"center"} mt={"30px"}>
-          <Heading mb={"50px"}>카테고리별 상품</Heading>
+          {/* ------------------- 검색바 ------------------- */}
+          <Box
+            display={"flex"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+            border={"1px solid gray"}
+            boxSizing="border-box"
+            mb={10}
+            w={"90%"}
+            h={"90px"}
+            ml={"5%"}
+            borderRadius={"20px"}
+            boxShadow={"0px 8px 15px #0000001A"}
+          >
+            <VStack alignItems={"flex-start"}>
+              <p
+                style={{
+                  fontSize: "12px",
+                  marginBottom: "-10px",
+                  marginLeft: "17px",
+                  color: "gray",
+                }}
+              >
+                여행지 숙소명
+              </p>
+              <Input
+                fontSize={"16px"}
+                w={"220px"}
+                border={"none"}
+                placeholder="숙소명 어디로 가시나요?"
+                style={{ fontSize: "15px" }}
+                _focus={{
+                  boxShadow: "none", // 포커스 시 박스 그림자 제거
+                }}
+              />
+            </VStack>
+
+            {/* 달력 구현 해야함 */}
+
+            <VStack alignItems={"flex-start"}>
+              <p
+                style={{
+                  fontSize: "12px",
+                  marginBottom: "-10px",
+                  color: "gray",
+                  marginLeft: "15px",
+                }}
+              >
+                숙박 날짜
+              </p>
+              <Box ref={wrapperRef} style={{ position: "relative" }}>
+                <DatePicker
+                  className="react-datepicker"
+                  locale={ko}
+                  selectsRange
+                  startDate={startDate}
+                  endDate={endDate}
+                  onChange={handleChange}
+                  customInput={<CustomInput />}
+                  popperPlacement="bottom"
+                  popperModifiers={{
+                    offset: {
+                      enabled: true,
+                      offset: "0px, 8px",
+                    },
+                    preventOverflow: {
+                      enabled: true,
+                      boundariesElement: "viewport",
+                    },
+                  }}
+                  dateFormat="yyyy.MM.dd(eee)" // 포맷 지정
+                  monthsShown={2} // 2달치 달력 보여줌
+                  formatWeekDay={(nameOfDay) => {
+                    switch (nameOfDay) {
+                      case "일요일":
+                        return "일";
+                      case "월요일":
+                        return "월";
+                      case "화요일":
+                        return "화";
+                      case "수요일":
+                        return "수";
+                      case "목요일":
+                        return "목";
+                      case "금요일":
+                        return "금";
+                      case "토요일":
+                        return "토";
+                      default:
+                        return nameOfDay;
+                    }
+                  }}
+                />
+              </Box>
+            </VStack>
+
+            {/* 성인, 소인 인원선택 구현해야함 */}
+            <div style={{ fontSize: "16px" }}>성인 2, 객실 1</div>
+            <Button bg={"#4caf50"} color={"white"} borderRadius={"20px"}>
+              검색하기
+            </Button>
+          </Box>
           <Card
             w={"400px"}
             h={"50px"}
