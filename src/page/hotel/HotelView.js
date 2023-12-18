29f746a873ae67bc6f67f3249b0a151cc6a75f44
CHOHUNE <yuureru@gmail.com>
2023-12-18 17:46:36 +0900

    12/18 호텔 뷰 페이지 내 탭 추가

--- src/page/hotel/HotelView.js
+++ src/page/hotel/HotelView.js
@@ -20,6 +20,11 @@ import {
   Textarea,
   Select,
   Card,
+  Tab,
+  TabList,
+  Tabs,
+  TabPanel,
+  TabPanels,
 } from "@chakra-ui/react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
@@ -353,38 +358,27 @@ export function HotelView() {
                     <Text ml={"10px"}>체크인: 15:00, 체크아웃: 11:00</Text>
                   </Box>
 
-                  {/*</Box>*/}
-
-                  <Flex justifyContent="center" gap="20px" mt="10px">
-                    <Box
-                      border="1px solid"
-                      borderColor="gray.400"
-                      cursor="pointer"
-                      w="150px"
-                      h="100px"
-                      borderRadius="10px"
-                      display="flex"
-                      textAlign="center"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Heading>예약하기</Heading>
+                  <Box
+                    mb={"20px"}
+                    h={"180px"}
+                    w={"400px"}
+                    borderRadius={"10px"}
+                    shadow={"1px 1px 3px 1px #dadce0 inset"}
+                    // alignItems={"center"}
+                    display={"flex"}
+                    flexDirection={"column"}
+                  >
+                    <Box ml={"10px"} mt={"10px"}>
+                      {/*<Box>{hotel.description}</Box>*/}
+                      <Box mb={"5px"}>
+                        {hotel.oceanview != null && "오션뷰"}
+                      </Box>
+                      <Box mb={"5px"}>{hotel.pool != null && "수영장"}</Box>
+                      <Box mb={"5px"}>
+                        {hotel.pet != null && "반려견 동반 가능"}
+                      </Box>
                     </Box>
-                    <Box
-                      border="1px solid"
-                      borderColor="gray.400"
-                      cursor="pointer"
-                      w="150px"
-                      h="100px"
-                      borderRadius="10px"
-                      display="flex"
-                      textAlign="center"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Heading>찜하기</Heading>
-                    </Box>
-                  </Flex>
+                  </Box>
                 </Flex>
 
                 {/*</Box>*/}
@@ -471,114 +465,163 @@ export function HotelView() {
                 </Text>
               </Flex>
             </Box>
-            <SimpleGrid columns={1} spacing={5} my="20px" mr="10%" w="80%">
-              {roomtypeList &&
-                roomtypeList.map((roomtype) => (
-                  <Box
-                    key={roomtype.hid}
-                    borderRadius="lg"
-                    border="1px solid"
-                    borderColor={"gray.400"}
-                    p="20px"
-                    display="flex"
-                    textAlign={"center"}
-                    alignItems={"center"}
-                    justifyContent={"center"}
-                    w={"1300px"}
-                    // maxWidth="600px" // Set the maximum width for the box
-                    // mx="auto" // Center the box horizontally
-                  >
-                    <Image
-                      ml={"20px"}
-                      src={roomtype.roomImgUrl}
-                      alt={roomtype.roomtype}
-                      maxW="300px" // Set maximum width for the image
-                      maxH="200px" // Set maximum height for the image
-                      borderRadius={"10px"}
-                    />
-                    <Text ml={"50px"} fontSize="xl" fontWeight="bold">
-                      {roomtype.roomtype}
-                    </Text>
-                    <Spacer />
-                    <VStack textAlign={"right"}>
-                      <Text> 객실 인원 :{roomtype.maxMinPerRoom}</Text>
-                      <Text> 취사 여부 :{roomtype.ableCooking}</Text>
-                      <Text> 침실 수 :{roomtype.numberOfBedRoom}</Text>
-                    </VStack>
-                    <Spacer />
-                    <VStack mr="40px">
-                      <Text fontSize="xl" fontWeight="bold">
-                        {/* 변경된 부분: 날짜가 선택되지 않았을 때는 평일 요금 표시 */}
-                        숙박 요금 :
-                        {roomTypePrices[roomtype.roomtype] &&
-                          roomTypePrices[roomtype.roomtype].toLocaleString(
-                            "ko-KR",
-                            {
-                              style: "currency",
-                              currency: "KRW",
-                            },
-                          )}
-                      </Text>
-                      <Text>
-                        {/* 변경된 부분: 날짜가 선택되지 않았을 때는 평일 요금 표시 */}
-                        부가세 · 봉사료 10% 포함 (세금계산서 · 현금영수증 발행)
-                      </Text>
-                      <Text color="red" fontWeight={"semibold"}>
-                        취소불가(취소수수료 발생)
-                      </Text>
-                    </VStack>
+            {/* 여기서 부터 객실 렌더링 */}
+            <Tabs isFitted variant="enclosed" mt={"40px"}>
+              <TabList mb="1em">
+                <Tab> 객실 </Tab>
+                <Tab>상세 설명</Tab>
+                <Tab>환불 및 유의사항</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <SimpleGrid columns={1} spacing={5} my="20px">
+                    {roomtypeList &&
+                      roomtypeList.map((roomtype) => (
+                        <Box
+                          key={roomtype.hid}
+                          borderRadius="lg"
+                          border="1px solid"
+                          borderColor={"gray.400"}
+                          p="10px"
+                          display="flex"
+                          textAlign={"center"}
+                          alignItems={"center"}
+                          justifyContent={"center"}
+                          ml={"30px"}
+                          w={"95%"}
+                          // maxWidth="600px" // Set the maximum width for the box
+                          // mx="auto" // Center the box horizontally
+                        >
+                          <Image
+                            ml={"20px"}
+                            src={roomtype.roomImgUrl}
+                            alt={roomtype.roomtype}
+                            maxW="300px" // Set maximum width for the image
+                            maxH="200px" // Set maximum height for the image
+                            borderRadius={"10px"}
+                          />
+                          <Text ml={"50px"} fontSize="xl" fontWeight="bold">
+                            {roomtype.roomtype}
+                          </Text>
+                          <Spacer />
+                          <VStack textAlign={"right"}>
+                            <Text> 객실 인원 :{roomtype.maxMinPerRoom}</Text>
+                            <Text> 취사 여부 :{roomtype.ableCooking}</Text>
+                            <Text> 침실 수 :{roomtype.numberOfBedRoom}</Text>
+                          </VStack>
+                          <Spacer />
+                          <VStack mr="40px">
+                            <Text fontSize="xl" fontWeight="bold">
+                              {/* 변경된 부분: 날짜가 선택되지 않았을 때는 평일 요금 표시 */}
+                              숙박 요금 :
+                              {roomTypePrices[roomtype.roomtype] &&
+                                roomTypePrices[
+                                  roomtype.roomtype
+                                ].toLocaleString("ko-KR", {
+                                  style: "currency",
+                                  currency: "KRW",
+                                })}
+                            </Text>
+                            <Text>
+                              {/* 변경된 부분: 날짜가 선택되지 않았을 때는 평일 요금 표시 */}
+                              부가세 · 봉사료 10% 포함 (세금계산서 · 현금영수증
+                              발행)
+                            </Text>
+                            <Text color="red" fontWeight={"semibold"}>
+                              취소불가(취소수수료 발생)
+                            </Text>
+                          </VStack>
 
-                    <Stack>
-                      <Select
-                        placeholder="객실 수 "
-                        onChange={(e) => setCount(e.target.value)}
-                      >
-                        <option value="1">객실 1개</option>
-                        <option value="2">객실 2개</option>
-                        <option value="3">객실 3개</option>
-                        <option value="4">객실 4개</option>
-                        <option value="5">객실 5개</option>
-                      </Select>
-                      <Button
-                        variant={"outline"}
-                        colorScheme={"green"}
-                        onClick={() => handleReservationClick(roomtype)}
-                      >
-                        {" "}
-                        예약하기{" "}
-                      </Button>
-                    </Stack>
+                          <Stack>
+                            <Select
+                              placeholder="객실 수 "
+                              onChange={(e) => setCount(e.target.value)}
+                            >
+                              <option value="1">객실 1개</option>
+                              <option value="2">객실 2개</option>
+                              <option value="3">객실 3개</option>
+                              <option value="4">객실 4개</option>
+                              <option value="5">객실 5개</option>
+                            </Select>
+                            <Button
+                              variant={"outline"}
+                              colorScheme={"green"}
+                              onClick={() => handleReservationClick(roomtype)}
+                            >
+                              {" "}
+                              예약하기{" "}
+                            </Button>
+                          </Stack>
+                        </Box>
+                      ))}
+                  </SimpleGrid>
+                </TabPanel>
+                {/* 상세 페이지 */}
+                <TabPanel>
+                  <Box
+                    w={"80%"}
+                    ml={"10%"}
+                    border={"1px solid "}
+                    borderColor="gray.400"
+                    h={"400px"}
+                    mb={"15px"}
+                    // display={"flex"}
+                    // justifyContent={"center"}
+                    // alignItems={"center"}
+                    borderRadius={"lg"}
+                    p={"10px"}
+                  >
+                    {hotel.description}
+                  </Box>{" "}
+                  {/*주의 사항 탭*/}
+                </TabPanel>
+                <TabPanel>
+                  <Box
+                    w={"80%"}
+                    ml={"10%"}
+                    border={"1px solid "}
+                    borderColor="gray.400"
+                    h={"400px"}
+                    mb={"15px"}
+                    // display={"flex"}
+                    // justifyContent={"center"}
+                    // alignItems={"center"}
+                    borderRadius={"lg"}
+                  >
+                    <Box ml={"10px"} mt={"10px"}>
+                      <Box>
+                        <Heading textColor={"red.400"}>
+                          취소/환불/변경 유의사항
+                        </Heading>
+                      </Box>
+                      <Box mt={"10px"}>
+                        <Text>{hotel.cautionMessage}</Text>
+                        <Text>
+                          - 예약 취소, 접수는 마이페이지를 통해 가능합니다. -
+                          취소수수료가 발생하는 경우에는 마이페이지 상에서
+                          불가능하오니 고객센터로 문의하시기 바랍니다. - 취소,
+                          변경 규정은 업무 시간 기준으로 적용되며 영업시간 이후
+                          취소, 변경은 익일 영업시간 기준으로 적용됩니다. -
+                          근무시간 이후에 현지 프론트로 예약을 취소하셔도 환불이
+                          불가능합니다. - 현지 프론트로 직접 취소하셔도
+                          취소접수만 받으며, 현지에서는 환불에 대한 책임이
+                          없습니다. - 당사에서 예약한 예약건은 취소 및 변경도
+                          당사로 통보하셔야 합니다. - 변경 및 취소 약관은 당사에
+                          준하며 숙박지사와는 무관하오니 고객님의 개별적인
+                          숙박지사와의 취소는 당사에서 책임지지 않습니다. - 입실
+                          후 개인사정에 의한 조기퇴실은 어떤 경우에라도 환불이
+                          되지 않습니다. - 취소, 변경은 본사 근무 시간에 맞춰져
+                          있습니다. - 예약취소 가능시간 월-금요일: 09:00-
+                          17:00까지 / 토.일 공휴일은 휴무입니다. - 취소, 변경은
+                          필히 당사로 하셔야 하며 유선으로만 접수합니다.
+                          메일접수는 받지 않습니다. - 전화 : 02-535-0114
+                        </Text>
+                      </Box>
+                    </Box>
                   </Box>
-                ))}
-            </SimpleGrid>
-            <Box
-              w={"80%"}
-              ml={"10%"}
-              border={"1px solid "}
-              color="gray.400"
-              h={"400px"}
-              mb={"15px"}
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              borderRadius={"lg"}
-            >
-              {hotel.description}
-            </Box>{" "}
-            <Box
-              w={"80%"}
-              ml={"10%"}
-              border={"1px solid "}
-              color="gray.400"
-              h={"400px"}
-              mb={"15px"}
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              borderRadius={"lg"}
-            >
-              {hotel.cautionMessage}
-            </Box>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </>
         )}
       </Box>
