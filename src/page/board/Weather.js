1acbbc9c6c6228c343f3861f72b572823ef6db1a
Jae Hee <travel237corp@naver.com>
2023-12-19 12:24:18 +0900

    해외 날씨 정복

--- src/page/board/Weather.js
+++ src/page/board/Weather.js
@@ -2,21 +2,25 @@ import {
   Box,
   Card,
   CardBody,
-  Divider,
   Flex,
-  Image,
   Heading,
+  Image,
   Input,
+  SimpleGrid,
   Stack,
   Text,
-  SimpleGrid,
 } from "@chakra-ui/react";
 import { LBanner } from "./Banner/LBanner";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { RBanner } from "./Banner/RBanner";
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { RMW } from "./weather/RMW";
+import { NYW } from "./weather/NYW";
+import { KRW } from "./weather/KRW";
+import { DUW } from "./weather/DUW";
+import { PAW } from "./weather/PAW";
+import { TKW } from "./weather/TKW";
 
 export function Weather() {
   const navigate = useNavigate();
@@ -24,7 +28,7 @@ export function Weather() {
   const [locaion, setLocaion] = useState("");
   const [result, setResult] = useState({});
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${locaion}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${locaion}&Gangwon-doappid=${API_KEY}`;
 
   const searchWeather = async (e) => {
     if (e.key === "Enter") {
@@ -72,108 +76,12 @@ export function Weather() {
               spacing={2}
               templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
             >
-              <Card maxW="sm">
-                <CardBody>
-                  <Image
-                    src="
-                    https://study1993garbi.s3.ap-northeast-2.amazonaws.com/travel/board/42/ferdinand-stohr-PeFk7fzxTdk-unsplash.jpg"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md"> 뉴욕 날씨</Heading>
-                    <Text color="blue.600" fontSize="2xl">
-                      32 °C
-                    </Text>
-                    <Text>Sunny </Text>
-                  </Stack>
-                </CardBody>
-              </Card>
-              <Card maxW="sm">
-                <CardBody>
-                  <Image
-                    src="
-                    https://study1993garbi.s3.ap-northeast-2.amazonaws.com/travel/board/42/cristina-gottardi-I1Lv2yX67GI-unsplash.jpg"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">로마 날씨</Heading>
-                    <Text color="blue.600" fontSize="2xl">
-                      25 °C
-                    </Text>
-                    <Text>Rain</Text>
-                  </Stack>
-                </CardBody>
-              </Card>
-
-              <Card maxW="sm">
-                <CardBody>
-                  <Image
-                    src="https://study1993garbi.s3.ap-northeast-2.amazonaws.com/travel/board/44/%ED%95%9C%EA%B5%AD.jpg"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">한국 날씨</Heading>
-                    <Text color="blue.600" fontSize="2xl">
-                      25 °C
-                    </Text>
-                    <Text>Rain</Text>
-                  </Stack>
-                </CardBody>
-              </Card>
-              <Card maxW="sm">
-                <CardBody>
-                  <Image
-                    src="
-                   https://study1993garbi.s3.ap-northeast-2.amazonaws.com/travel/board/43/dan-calderwood-PBokKdfU7ic-unsplash.jpg"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">두바이 날씨</Heading>
-                    <Text color="blue.600" fontSize="2xl">
-                      25 °C
-                    </Text>
-                    <Text>Rain</Text>
-                  </Stack>
-                </CardBody>
-              </Card>
-              <Card maxW="sm">
-                <CardBody>
-                  <Image
-                    src="
-                    https://study1993garbi.s3.ap-northeast-2.amazonaws.com/travel/board/43/ilnur-kalimullin-CB0Qrf8ib4I-unsplash.jpg"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">프랑스 날씨</Heading>
-                    <Text color="blue.600" fontSize="2xl">
-                      25 °C
-                    </Text>
-                    <Text>Rain</Text>
-                  </Stack>
-                </CardBody>
-              </Card>
-              <Card maxW="sm">
-                <CardBody>
-                  <Image
-                    src="
-                   https://study1993garbi.s3.ap-northeast-2.amazonaws.com/travel/board/44/%EC%9D%BC%EB%B3%B8.jpg"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">일본 날씨</Heading>
-                    <Text color="blue.600" fontSize="2xl">
-                      25 °C
-                    </Text>
-                    <Text>Rain</Text>
-                  </Stack>
-                </CardBody>
-              </Card>
+              <NYW />
+              <RMW />
+              <KRW />
+              <DUW />
+              <PAW />
+              <TKW />
             </SimpleGrid>
           </Box>
 
